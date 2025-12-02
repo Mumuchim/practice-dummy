af6056dc273992248c45b1b4b6e06533420c9cdc
Jerome Isaac Cereneo <mumuchim.jic@gmail.com>
2025-12-02 22:55:24 +0800

    new

--- 4_!00p.py
+++ 4_!00p.py
@@ -6,7 +6,7 @@ bracelet_pearl = 2000
 bracelet_diamond = 30000
 
 tags = [bracelet_wood, bracelet_pearl, bracelet_diamond]
-total = 0
-for price in tags:
-    total += price
+total = 0  # eto ang base
+for price in tags:  # new for loop called "price" calling tags array
+    total += price  # total base + current loop in array 1, then loop to array 2, then loop to array 3
 print(f"Total: {total}")
