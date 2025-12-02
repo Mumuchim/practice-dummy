afe84c34fa21328cedcbbcd289ca3d85ef79db60
Jerome Isaac Cereneo <mumuchim.jic@gmail.com>
2025-12-02 22:20:35 +0800

    deleted demo.ipynb

--- 4_!00p.py
+++ 4_!00p.py
@@ -1,5 +1,12 @@
-prices = [10, 20, 30]
+
+# MUMUCHIM bracelet store
+
+bracelet_wood = 30
+bracelet_pearl = 2000
+bracelet_diamond = 30000
+
+tags = [bracelet_wood, bracelet_pearl, bracelet_diamond]
 total = 0
-for price in prices:
+for price in tags:
     total += price
-print({total})
+print(f"Total: {total}")
